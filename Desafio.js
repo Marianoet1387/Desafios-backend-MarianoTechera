bf13680fe471dd3760037e5b0c377a34c3785d5e
mariano techera <marianoeduardotechera@gmail.com>
2023-09-17 17:15:16 +0200

    V0.0

--- Desafio.js
+++ Desafio.js
@@ -7,7 +7,11 @@ class ProductManager {
         return this.Products
     }
     addProduct(title, description, price, thumbnail, code, stock) {
-        this.Products.push(
+        let product = this.Products.find(prod => prod.code === code)
+        if (product) {
+            return console.log("ya esta en la lista")
+        }else{
+              this.Products.push(
             {
                 id: this.Products.length + 1,
                 title,
@@ -17,25 +21,29 @@ class ProductManager {
                 code,
                 stock
             })
-        console.log("Prodcut added")
+        
+        }console.log("Prodcut added")
+      
     }
     getProdcutById(id) {
         let product = this.Products.find(prod => prod.id === id)
         if (!product) {
             console.log("Product not found")
+        }else {
+            console.log("product found",product)
         }
     }
+}
 
-    }
-    let productManager = new ProductManager();
+let productManager = new ProductManager();
 
+console.log("getProdcuts",productManager.getProdcuts());
+
+productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25)
 console.log(productManager.getProdcuts());
 
 productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25)
-console.log(productManager.getProdcuts());
 
-productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25)
+productManager.getProdcutById(4);
 
-productManager.getProdcutById();
-
-console.log(productManager.getProdcuts());
+console.log("getProdcuts",productManager.getProdcuts());
